47cdb9f8196fd9d93d3e123d8a25f2635ef85144
muharemsahin <59576382+muharemsahin@users.noreply.github.com>
2023-06-14 20:35:22 +0200

    Update index.js

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,14 @@
-const { Pool } = require("pg");
- 
-const pool = new Pool();
- 
-module.exports ={
-    query : (text, params) => pool.query(text, params),
-} 
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
